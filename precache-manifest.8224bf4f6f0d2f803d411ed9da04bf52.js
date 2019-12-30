0d86cb52a655670dfa962bc6add79e4a747a068a
yhomz <rit_blossom@naver.com>
2019-12-30 20:49:08 +0900

    Updates

---
+++ precache-manifest.8224bf4f6f0d2f803d411ed9da04bf52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1d0b282ec540ccff7c403229146087",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be8b84636f6ae69517fa",
+    "url": "./static/css/main.cb8e3745.chunk.css"
+  },
+  {
+    "revision": "b0a765586dd4a73f51e8",
+    "url": "./static/js/2.763918a1.chunk.js"
+  },
+  {
+    "revision": "be8b84636f6ae69517fa",
+    "url": "./static/js/main.75d4f27d.chunk.js"
+  },
+  {
+    "revision": "7d14d4887244df99c6fd",
+    "url": "./static/js/runtime-main.7f76d748.js"
+  }
+]);
